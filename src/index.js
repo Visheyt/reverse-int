a11bba56122a112c6ff327e981864a25b0057987
Alex <fornikaras7@gmail.com>
2023-08-06 10:33:52 +0500

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    const number = String(n);
+    let newStr = ""
+    if (number.startsWith("-")) {
+      const newNumber = number.slice(1);
+    for (let i = newNumber.length -1;i>=0 ; i--) {
+      newStr+=newNumber[i]
+    }
+    return Number(newStr)
+    }
+    else {
+      for (let i = number.length -1;i>=0 ; i--) {
+      newStr+=number[i]
+    }
+    return Number(newStr)
+    }
 }
